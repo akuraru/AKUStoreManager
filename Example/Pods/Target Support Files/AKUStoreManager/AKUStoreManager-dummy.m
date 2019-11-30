a355269bd0602b96b39172564960df1c22ed9126
akuraru <akuraru@gmail.com>
2019-12-01 00:08:45 +0900

    pod lib create

---
+++ Example/Pods/Target Support Files/AKUStoreManager/AKUStoreManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AKUStoreManager : NSObject
+@end
+@implementation PodsDummy_AKUStoreManager
+@end
---
+++ Example/Pods/Target Support Files/Pods-AKUStoreManager_Tests/Pods-AKUStoreManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AKUStoreManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AKUStoreManager_Tests
+@end
