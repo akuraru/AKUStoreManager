2a25d44f1f8cc08a39dd88e7fba0e75e393c5f85
akuraru <akuraru@gmail.com>
2019-12-01 20:49:30 +0900

    試験用のコードを追加

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -14,15 +14,59 @@ class Tests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        let searchPathDirectories: [FileManager.SearchPathDirectory] =  [
+            .applicationDirectory,
+            .demoApplicationDirectory,
+            .developerApplicationDirectory,
+            .adminApplicationDirectory,
+            .libraryDirectory,
+            .developerDirectory,
+            .userDirectory,
+            .documentationDirectory,
+            .documentDirectory,
+            .coreServiceDirectory,
+            .autosavedInformationDirectory,
+            .desktopDirectory,
+            .cachesDirectory,
+            .applicationSupportDirectory,
+            .downloadsDirectory,
+            .inputMethodsDirectory,
+            .moviesDirectory,
+            .musicDirectory,
+            .picturesDirectory,
+            .printerDescriptionDirectory,
+            .sharedPublicDirectory,
+            .preferencePanesDirectory,
+            .itemReplacementDirectory,
+            .allApplicationsDirectory,
+            .allLibrariesDirectory,
+            .trashDirectory
+        ]
+        
+        let domainMasks: [FileManager.SearchPathDomainMask] = [
+            .userDomainMask,
+            .localDomainMask,
+            .networkDomainMask,
+            .systemDomainMask,
+            .allDomainsMask
+        ]
+        let fileManager = FileManager()
+        for domainMask in domainMasks {
+            for searchPathDirectory in searchPathDirectories {
+                let urls = fileManager.urls(for: searchPathDirectory, in: domainMask)
+                print("searchPath: \(searchPathDirectory), domain: \(domainMask), url: \(urls.first)")
+            }
+        }
+        
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
+        print(NSUserName())
+        print(NSFullUserName())
+        print(NSHomeDirectory())
+        print(NSHomeDirectoryForUser("hoge"))
+        print(NSTemporaryDirectory())
+        print(NSOpenStepRootDirectory())
     }
     
 }
